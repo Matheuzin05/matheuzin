ea0477218410a896a9be9283800480f0883c087e
Matheuzin05 <correarui.msn@gmail.com>
2025-06-18 08:29:01 -0300

    18/06

--- app.py
+++ app.py
@@ -6,26 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/login', methods=['POST'])
-def login():
-    username = request.form['username']
-    password = request.form['password']
-   
-    if username == '' and password == '123':
-        return redirect('/servicos')
-    else:
-        print('Usuário não encontrado')
-        return "Usuário ou senha incorretos", 401
-
-
 @app.route('/login')
-def cadastro():
+def login():
     return render_template('login.html')
 
 @app.route('/servicos')
 def servicos():
     return render_template('servicos.html')
 
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
